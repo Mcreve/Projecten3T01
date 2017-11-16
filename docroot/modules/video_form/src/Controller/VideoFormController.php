5f9fc2b5503b6503631ed65ed1052b1a74791a8b
Natasha <natasha.heinkens@student.hogent.be>
2017-11-16 23:32:29 +0100

    modules. Video_form is onafgewerkte module (werkt niet)

---
+++ docroot/modules/video_form/src/Controller/VideoFormController.php
@@ -0,0 +1,12 @@
+<?php
+ 
+namespace Drupal\video_form\Controller;
+
+class VideoFormController {
+ public function vform() {
+ return array(
+                '#title' => 'Just testing',
+                '#markup' => 'Testing text',
+            );
+    }
+}
---
+++ docroot/modules/video_form/src/Form/VideoForm.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\video_form\form\VideoForm;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class VideoForm extends FormBase {
+
+    public function getFormId() {
+        return 'amazing_forms_contribute_form';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(array $form, FormStateInterface $form_state) {
+        
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validateForm(array &$form, FormStateInterface $form_state) {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function submitForm(array &$form, FormStateInterface $form_state) {
+       
+    }
+}
